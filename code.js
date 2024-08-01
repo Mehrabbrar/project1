3374f2b95cfe29760834a22286bd94480b08cbd4
Mehrab Brar <mehrabbrar786@gmail.com>
2024-07-31 19:44:51 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,67 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.header {
+    background-color: blue
+    text-align: center;
+    padding: 20px;
+}
+  
+header he {
+    color:white;
+    font-size: 40px;
+}
+
+.search-bar {
+    margin: 20px auto;
+    width:50%;
+    text-align: center;
+}
+
+input {
+    width: 50%;
+    padding: 10px;
+    border: 1px solid #333333;
+    font-size: 18px;
+    border-radius: 8px;
+}
+form input{
+    flex: 1;
+    height: 100%;
+    border: 0;
+    outline: 0;
+    background: transparent;
+    color: #fff
+    font-size; 18px;
+    padding: 0 30px;
+}
+form button{
+    padding:0 40px;
+    height: 100%;
+    background: #ff3929;
+    color: #fff;
+    font-size: 18px;
+    border: 0;
+    outline: 0;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+}
+::placeholder{
+    color: #fff;
+    font-size: 18px;
+}
+#show-more-btn{
+    background: #ff3929;
+    color: #fff;
+    border: 0;
+    outline: 0;
+    padding: 10px 20px;
+    border-radius: 4px;
+    margin: 10px auto 100px;
+    cursor: pointer;
+    display: none;
+}
